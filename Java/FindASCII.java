924cc87aa2c39f90ca026176bbcb65e0bb940ae9
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2020-07-29 17:52:16 +0530

    To find the ascii values of the character

---
+++ Java/FindASCII.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FindAsciiVal {
+    public static void main(String[] agrs){
+        Scanner input = new Scanner(System.in);
+        char character;
+        
+        System.out.println("Enter the character of which you want ASCII value: ");
+        character = input.next().charAt(0);
+        
+        int ascii = (int) character;
+
+        System.out.println("The ASCII value for " + character + " is " + ascii);
+    }
+}
