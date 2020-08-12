c61a80e8f8e757d019d288c299bb238ec3f1ef2b
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2020-08-12 13:08:03 +0530

    Evaluate Postfix expression

--- Data Structures/Postfix.cpp
+++ Data Structures/Postfix.cpp
@@ -1,69 +1,75 @@
 #include<iostream>
-#include<string>
 #include<stack>
+#include<string>
+
 using namespace std;
 
-int evaluate(string a){
+int EvaluatePostfix(string exp);
+int PerformOperation(char operation, int operand1, int operand2);
+bool IsOperator(char C);
 
-    stack<char> S;
-    int x, y, z;
-
-    for(int i; i < a.length(); i++){
-        
-        if(a[i] == ' '  || ',') continue;
-        else if(a[i] != '+' || '-' || '*' || '/'){
-            S.push(a[i]);
-        }
-        else if(a[i] == '+'){
-            x = S.top();
-            S.pop();
-            y = S.top();
-            S.pop();
-            z = x + y;
-            S.push(z);
-            }
-        else if(a[i] == '-'){
-            x = S.top();
-            S.pop();
-            y = S.top();
-            S.pop();
-            z = x - y;
-            S.push(z);
-            }
-        else if(a[i] == '*'){
-            x = S.top();
-            S.pop();
-            y = S.top();
-            S.pop();
-            z = x + y;
-            S.push(z);
-            }
-        else if(a[i] == '/'){
-            x = S.top();
-            S.pop();
-            y = S.top();
-            S.pop();
-            z = x + y;
-            S.push(z);
-            }
-    }
-
-    return S.top();
-
-
-}
+bool IsNumericDigit(char C);
 
 int main(){
-    
-    string a;
+	string exp; 
+	cout<<"Enter Postfix Expression \n";
+	getline(cin,exp);
+	int result = EvaluatePostfix(exp);
+	cout<<"Output = "<<result<<"\n";
+}
 
-    cout<<"Enter an expression: "<<endl;
-    cin>>a;
 
-    evaluate(a);
+int EvaluatePostfix(string exp){
+
+	stack<int> S;
+
+	for(int i = 0;i< exp.length();i++) {
+
+		 
+		if(exp[i] == ' ' || exp[i] == ',') continue; 
+ 
+		else if(IsOperator(exp[i])) {
+			
+			int operand2 = S.top(); S.pop();
+			int operand1 = S.top(); S.pop();
+			int result = PerformOperation(exp[i], operand1, operand2);
+			S.push(result);
+		}
+		else if(IsNumericDigit(exp[i])){
+			 
+			int operand = 0; 
+			while(i<exp.length() && IsNumericDigit(exp[i])) {
+				 operand = (operand*10) + (exp[i] - '0'); 
+				i++;
+			} 
+			i--;
+			S.push(operand);
+		}
+	} 
+	return S.top();
+}
 
 
-    
-    
-    return 0;
+bool IsNumericDigit(char C){
+	if(C >= '0' && C <= '9') return true;
+	return false;
+}
+
+
+bool IsOperator(char C){
+	if(C == '+' || C == '-' || C == '*' || C == '/')
+		return true;
+
+	return false;
+}
+
+
+int PerformOperation(char operation, int operand1, int operand2){
+	if(operation == '+') return operand1 +operand2;
+	else if(operation == '-') return operand1 - operand2;
+	else if(operation == '*') return operand1 * operand2;
+	else if(operation == '/') return operand1 / operand2;
+
+	else cout<<"Unexpected Error \n";
+	return -1; 
 }
