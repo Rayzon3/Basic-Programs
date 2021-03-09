7fe7a9e048b6f23be4fc1fff5b3a1bc8f11ac321
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2021-03-09 17:53:47 +0530

    Updated, better approach

--- R/QuickSort.r
+++ R/QuickSort.r
@@ -1,33 +1,41 @@
 arr = c(2, 4, 1, 5, 3, 9)
 
-quickSort <- function(arr) {
-  # Pick a number at random.
-  mid <- sample(arr, 1)
+quickSort <- function(arr){
+  len = length(arr)
+  if(len <= 1){
+    return(arr)
+  }
+  else{
+    # pivot will be the last element
+    pivot = arr[len]
+  }
+  #Making partition arrays
+  elements_greater <- c()
+  elements_lesser <- c()
   
-  # Place-holders for left and right values.
-  left <- c()
-  right <- c()
-  
-  # Move all the smaller values to the left, bigger values to the right.
-  lapply(arr[arr != mid], function(d) {
-    if (d < mid) {
-      left <<- c(left, d)
+  #Sorting the elements according to pivot
+  lapply(arr[arr != pivot], function(element){
+    if(element > pivot){
+      elements_greater <<- c(elements_greater, element)
     }
-    else {
-      right <<- c(right, d)
+    else{
+      elements_lesser <<- c(elements_lesser, element)
     }
   })
   
-  if (length(left) > 1) {
-    left <- quickSort(left)
+  #Making recursive calls to sort the partitions created
+  if(length(elements_greater) > 1){
+    elements_greater <- quickSort(elements_greater)
+  }
+  if(length(elements_lesser) > 1){
+    elements_lesser <- quickSort(elements_lesser)
   }
   
-  if (length(right) > 1) {
-    right <- quickSort(right)
-  }
-  
-  
-  return(c(left, mid, right))
+  return(c(elements_lesser, pivot, elements_greater))
 }
 
 print(quickSort(arr))
+
+
+
+
