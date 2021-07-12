bcb1d2b928f5663213c015c63827a677210db265
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2021-07-12 09:44:09 +0530

    Find the number of ways to make target string from given string array

--- DynamicProgramming/countConstruct.java
+++ DynamicProgramming/countConstruct.java
@@ -11,6 +11,34 @@ public class countConstruct {
                 total += numWays;
             }
         }
+import java.util.HashMap;
+
+public class countConstruct {
+
+    static HashMap<String, Integer> memo = new HashMap<>();
+    public static int countConstructString(String target, String[] wordBank){
+        if(memo.get(target) != null){
+            return memo.get(target);
+        }
+        if(target == "") return 1;
+        int total = 0;
+        for(String word : wordBank){
+            if(target.indexOf(word) == 0){
+                String suffix = target.substring(word.length());
+                int numWays = countConstructString(suffix, wordBank);
+                total += numWays;
+            }
+        }
+
+        memo.put(target, total);
+        return total;
+    }
+
+    public static void main(String[] args) {
+        String[] wordBank = {"purp", "p", "ur", "le", "purpl"};
+        System.out.println(countConstructString("purple", wordBank));
+    }
+}
 
         return total;
     }
