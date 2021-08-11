f421161d64b120afd527e27fb7fbc63034eea1cc
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2021-08-11 06:45:15 +0530

    updated

--- AlgoDesign/mergeSort.cpp
+++ AlgoDesign/mergeSort.cpp
@@ -1,54 +1,83 @@
+//Merge Sort
 #include<iostream>
-
 using namespace std;
 
-void merge(int *A, int *L, int *R, int leftCount, int rightCount){
+void merge(int A[], int l, int r, int m){
+    int lengthOfSubArray_1 = m - l + 1;
+    int lengthOfSubArray_2 = r - m;
 
-    int i,j,k;
-    i = 0;
-    j = 0; 
-    k = 0;
+    //temp arrays
+    int L[lengthOfSubArray_1];
+    int R[lengthOfSubArray_2];
 
-    while(i < leftCount && j < rightCount){
-        if(L[i] < R[j]) A[k++] = L[i++];
-        else A[k++] = R[j++];
+    for(int i = 0; i < lengthOfSubArray_1; i++){
+        L[i] = A[l + i];
+    }     
+    for(int j = 0; j < lengthOfSubArray_2; j++){
+        R[j] = A[m + 1 + j];
     }
-    while(i < leftCount) A[k++] = L[i++];
-	while(j < rightCount) A[k++] = R[j++];
+
+    //merge to [l....r]
+    int i = 0; //1st subarray
+    int j = 0; //2nd subarray
+    int k = l; //merged subarray
+
+    while(i < lengthOfSubArray_1 && j < lengthOfSubArray_2){
+        if(L[i] <= R[j]){
+            A[k] = L[i];
+            i++;
+        }else{
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    //copy remaining elements from R[] and L[]
+    while(i < lengthOfSubArray_1){
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while(j < lengthOfSubArray_2){
+        A[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void mergeSort(int *A, int n){
-    int mid = n/2;
-    
-    if(n < 2) return;
+void mergeSort(int A[], int l, int r){
+    if(l < r){
+        int m = l + (r - l)/2;
 
-    mid = n/2;
-    //creating left and right sub-arrays
-    int *L = (int*)malloc(mid*sizeof(int));
-    int *R = (int*)malloc((n - mid)*sizeof(int));
-    //populate
-    for(int i = 0; i < mid; i++) L[i] = A[i];
-    for(int i = mid; i < n; i++) R[i] = A[i];
+        mergeSort(A, l, m);
+        mergeSort(A, m + 1, r);
 
-    //sorting left sub-array
-    mergeSort(L, mid);
-    //sorting right sub-array
-    mergeSort(R, n - mid);
-    //Merging
-    merge(A, L, R, mid, n - mid);
-    free(L);
-    free(R);
+        merge(A, l, r, m);
+    }
+}
+
+void print(int A[], int size){
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
 }
 
 int main(){
+    int A[] = {10, 2, 3, 1, 12};
+    int size = sizeof(A)/sizeof(A[0]);
 
-    int A[] = {6,2,3,1,9,10,15,13,12,17}; 
+    printf("Unsorted Array: ");
+    print(A, size);
+    printf("\n");
 
-    int numberOfElements = sizeof(A)/sizeof(A[0]); 
+    mergeSort(A, 0, size - 1);
 
-    mergeSort(A,numberOfElements);
+    printf("Sorted Array: ");
+    print(A, size);
+    printf("\n");
 
-	for(int i = 0; i < numberOfElements; i++) printf("%d ",A[i]);
-
-    return 0; 
+    return 0;
 }
