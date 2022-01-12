1eaaf62a9c1def7d07e940a3139cec30637b708b
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2022-01-12 11:44:27 +0530

    Update db.pl

--- AI techiques and methods/Prolog/db.pl
+++ AI techiques and methods/Prolog/db.pl
@@ -3,5 +3,13 @@
 loves(romeo, juliet).
 loves(juliet, romeo) :- loves(romeo, juliet).
 
+[X, Y, Z] = [kirito, likes, asuna]
+/*  
+so here,
+X = kirito
+Y = likes
+Z = asuna
+*/
+
 /* load the file using -> [filename]. */
 /* then query from data/info */
